a3668c3235876812087fa69e22fa61e4d10f8564
621 <ljh7a21@gmail.com>
2024-01-17 11:32:35 +0900

    Click Image Slider : #photo .slider 영역 이미지 슬라이딩 기능 구현

--- EliceCroquetPage.js
+++ EliceCroquetPage.js
@@ -69,3 +69,64 @@ for (var i = 0; i < items.length; i++) {
         this.classList.add("active");
     }
 }
+
+/**
+ * Click Image Slider
+ * #photo .slider
+ * 화면 하단 좌우 버튼 클릭 시 이미지 슬라이드 기능 구현
+ */
+
+// 오른쪽 버튼 클릭 시 이미지 슬라이드
+document.querySelector(".right-arrow").onclick = function () {
+    var currentSlide = document.querySelector("#photo .slide.active");
+    var nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide === null) {
+        nextSlide = currentSlide.parentElement.firstElementChild;
+    }
+    currentSlide.animate({
+        opacity: [1, 0]
+    }, {
+        duration: 500,
+        easing: "ease",
+        iterations: 1,
+        fill: "both"
+    });
+    currentSlide.classList.remove("active");
+    nextSlide.animate({
+        opacity: [0, 1]
+    }, {
+        duration: 500,
+        easing: "ease",
+        iterations: 1,
+        fill: "both"
+    });
+    nextSlide.classList.add("active");
+}
+
+//왼쪽 이미지 슬라이드 기능 구현
+document.querySelector(".left-arrow").onclick = function () {
+    var currentSlide = document.querySelector("#photo .slide.active");
+    var preSlide = currentSlide.previousElementSibling;
+    if (preSlide === null) {
+        preSlide = currentSlide.parentElement.lastElementChild;
+    }
+    currentSlide.animate({
+        opacity: [1, 0]
+    }, {
+        duration: 500,
+        easing: "ease",
+        iterations: 1,
+        fill: "both"
+    });
+    currentSlide.classList.remove("active");
+    preSlide.animate({
+        opacity: [0, 1]
+    }, {
+        duration: 500,
+        easing: "ease",
+        iterations: 1,
+        fill: "both"
+    });
+    preSlide.classList.add("active");
+}
+
